2bc178ef9ea0e320a34d50300d99ecd84a0b47d0
Chase Dunagan <cdunagan@utexas.edu>
2017-03-10 09:44:04 -0700

    create new rails app and add signup test

---
+++ spec/features/guest_can_sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "guest can sign up" do
+  scenario "guest can sign up if they dont have an account" do
+    visit '/'
+
+    click_on "Sign Up"
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in "user_email", with: "chase@gmail.com"
+    fill_in "user_password", with: "newkid"
+    fill_in "user_password_confirmation", with: "newkid"
+    click_on "Submit"
+
+    expect(current_path).to eq(root_path)
+    expect(User.all.first.email).to eq("chase@gmail.com")
+  end
+end
