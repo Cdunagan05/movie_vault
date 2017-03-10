a8e22c72bfb96257ada84ffb800b6e73bac3e3c5
Chase Dunagan <cdunagan@utexas.edu>
2017-03-10 16:50:15 -0700

    add filtering buttons for watched and unwatched

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -16,4 +16,38 @@ $(document).ready(function(){
       }
     }
   })
+
+  $("#filter-by-watched").on("click", function(){
+    var filter = 'true'
+    var table = document.getElementById("movie-table-body")
+    var tr = table.getElementsByTagName("tr")
+
+    for (var i = 0; i < tr.length; i++) {
+      var td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        if (td.innerHTML.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  })
+
+  $("#filter-by-unwatched").on("click", function(){
+    var filter = 'false'
+    var table = document.getElementById("movie-table-body")
+    var tr = table.getElementsByTagName("tr")
+
+    for (var i = 0; i < tr.length; i++) {
+      var td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        if (td.innerHTML.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  })
 })
