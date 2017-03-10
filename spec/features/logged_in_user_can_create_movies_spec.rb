00dfcf246dbbb397ff94c7c42b26529a489f1d0d
Chase Dunagan <cdunagan@utexas.edu>
2017-03-10 11:11:22 -0700

    add user can create movie test

---
+++ spec/features/logged_in_user_can_create_movies_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "When a logged in user goes to their movies index" do
+  scenario "they can create a movie" do
+    user = stub_logged_in_user
+
+    visit '/'
+
+    fill_in "Title", with: "Shawshank Redemption"
+    fill_in "Note", with: "This is my favorite movie"
+    click_on "Create Movie"
+
+    expect(user.movies.first.title).to eq("Shawshank Redemption")
+    expect(user.movies.first.note).to eq("This is my favorite movie")
+  end
+end
