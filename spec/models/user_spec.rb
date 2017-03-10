764d18f495b5079e29ae43bdf463171e1a3e6fd3
Chase Dunagan <cdunagan@utexas.edu>
2017-03-10 10:22:09 -0700

    add user model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:email) }
+end
